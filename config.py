7fb340e1a1b9d848ac6fc20b4d3aeba200877259
Varshith Reddy <varshithreddymutra2006@gmail.com>
2026-04-19 20:54:40 +0530

    Updated Commit

---
+++ config.py
@@ -0,0 +1,8 @@
+CSV_PATH        = "Data/features_30_sec.csv"
+MODEL_SAVE_PATH = "saved_model/genre_model.keras"
+SCALER_PATH     = "saved_model/scaler.pkl"
+
+GENRES = ["blues", "classical", "country", "disco", "hiphop",
+          "jazz", "metal", "pop", "reggae", "rock"]
+
+EPOCHS = 25
